7b2fccd83d7076ed0012cb64ea6fcfef051dcc44
Greg London <gregdlondon@gmail.com>
2017-09-27 21:33:07 -0500

    Created validate_int function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#import sys
-#import os
+import sys
+import os
 
 def menu():
     print('1. View current trades')
@@ -9,4 +9,26 @@ def menu():
     print('5. View wins to losses')
     print('6. View menu')
 
+def validate_int(value):
+    while True:
+        try:
+            value = int(value)
+        except ValueError:
+            return False
+        else:
+            break
+    if value < 1 or value > 6:
+        return False
+    else:
+        return value
+
 menu()
+
+option = input('Choose option')
+entered = validate_int(option)
+
+if(entered != False):
+    print('good')
+    print('You entered', entered)
+else:
+    print('Not a vaild option')
