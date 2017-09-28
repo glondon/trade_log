dca8039b2c36c36ed73e9234a3180a63efc99d2b
Greg London <gregdlondon@gmail.com>
2017-09-27 19:49:01 -0500

    Created basic menu function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import sys
+#import sys
+#import os
+
+def menu():
+    print('1. View current trades')
+    print('2. Add new trade')
+    print('3. Update a trade')
+    print('4. Remove a trade')
+    print('5. View wins to losses')
+    print('6. View menu')
+
+menu()
