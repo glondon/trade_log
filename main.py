854f2f5a95d8cfec2fa9aa600d66ac24309a70a2
Greg London <gregdlondon@gmail.com>
2017-10-14 15:44:31 -0500

    Put menu items in list and looped through

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import sys
 import os
 
 def menu():
-    print('1. View current trades')
-    print('2. Add new trade')
-    print('3. Update a trade')
-    print('4. Remove a trade')
-    print('5. View wins to losses')
-    print('6. View menu')
+    menu_list = ['1. View current trades',
+                 '2. Add new trade',
+                 '3. Update a trade',
+                 '4. Remove a trade',
+                 '5. View wins to losses',
+                 '6. View menu']
+
+    for item in menu_list:
+        print(item)
 
 def validate_int(value):
     while True:
