229c67c876c6e7d8bc173691173e46c97610a830
Greg London <gregdlondon@gmail.com>
2017-11-16 19:54:40 -0600

    Added dollar symbols before values

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ class TradeLog:
                 print('Trades exited early: ' + str(exit_early[0]) + ' Good exits: ' + str(exit_early[1]))
                 print('Wins: ' + str(win_rate[0]) + ' Losses: ' + str(win_rate[1]) + ' Win Rate: ' + str(round(win_rate[2], 2)) 
                     + '%' + ' Average: $' + str(round(win_rate[3], 2)))
-                print('Largest Profit: ' + str(win_rate[4]) + ' Largest Loss: ' + str(win_rate[5]))
+                print('Largest Profit: $' + str(win_rate[4]) + ' Largest Loss: $' + str(win_rate[5]))
                 print('Open trades: ' + str(status_sum[0]) + ' Closed trades: ' + str(status_sum[1]))
                 print('Accounts: TOS: ' + str(acc_sum[0]) + ' IBG: ' + str(acc_sum[1]) + ' IBC: ' + str(acc_sum[2]))
             else:
