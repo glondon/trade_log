debbc5affd282076213e2ecd1d722e9e7b497fd5
Greg London <gregdlondon@gmail.com>
2018-03-26 19:32:54 -0500

    #150 fixed exit date and order by in closed trade view

--- main.py
+++ main.py
@@ -306,17 +306,18 @@ class TradeLog:
             else:
                 if m == 'y':
                     query += " AND exit_date <= '" + str(end) + "'"
-        else:
-            query += " WHERE status = 'open'"
 
-        query += " ORDER BY entry_date DESC"
+            query += " ORDER BY exit_date DESC"
+            
+        else:
+            query += " WHERE status = 'open' ORDER BY entry_date DESC"
 
         try:
             cur = self.db.cursor()
             cur.execute(query)
             if cur.rowcount > 0:
                 if o == False:
-                    format_header = '{0:3} {1:<6} {2:<6} {3:<10} {4:<5} {5:<5} {6:<8} {7:<8} {8:<8}'.format('ID', 'SYMBOL', 'POS', 'EX:DATE', 'ACC', 'COM', 'RESULT', 'STATUS', 'EX:DATE')
+                    format_header = '{0:3} {1:<6} {2:<6} {3:<10} {4:<5} {5:<5} {6:<8} {7:<8} {8:<8}'.format('ID', 'SYMBOL', 'POS', 'EX:DATE', 'ACC', 'COM', 'RESULT', 'STATUS', 'EN:DATE')
                 else:
                     format_header = '{0:3} {1:<6} {2:<6} {3:<10} {4:<5} {5:<5} {6:<8} {7:<8} {8:<8} {9:<8}'.format('ID', 'SYMBOL', 'POS', 'EN:DATE', 'ACC', 'COM', 'RESULT', 'STATUS', 'EN:PRICE', 'STOP')
                 print(format_header)
@@ -352,7 +353,7 @@ class TradeLog:
                             b_results.append(row[13]) 
 
                     if o == False:
-                        print_row = '{0:<3d} {1:<6} {2:<6} {3:<8} {4:<5} {5:<5f} {6:<8f} {7:<8} {8:<8}'.format(row[0], row[1], row[4], str(row[8]), row[10], comm, row[13], row[17], str(row[8]))
+                        print_row = '{0:<3d} {1:<6} {2:<6} {3:<8} {4:<5} {5:<5f} {6:<8f} {7:<8} {8:<8}'.format(row[0], row[1], row[4], str(row[8]), row[10], comm, row[13], row[17], str(row[7]))
                     else:
                         print_row = '{0:<3d} {1:<6} {2:<6} {3:<8} {4:<5} {5:<5f} {6:<8f} {7:<8} {8:<8} {9:<8}'.format(row[0], row[1], row[4], str(row[7]), row[10], comm, row[13], row[17], utils.format_price(row[2]), utils.format_price(row[5]))
                     print(print_row)    
