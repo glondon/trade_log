3c187e9c77181af1c38b73e0065965ece5561492
glondon <gregdlondon@gmail.com>
2018-12-16 12:16:29 -0600

    #238 set up Django main app files

---
+++ app/log/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Logs
+
+# Create your views here.
+def index(request):
+    #return HttpResponse('Test')
+    trades = Logs.objects.all()[:10]
+    data = {
+        'title': 'Logs',
+        'trades': trades
+    }
+    return render(request, 'log/index.html', data)
+
+def details(request, id):
+    #TODO validate id
+    trade = Logs.objects.get(id=id)
+    data = {
+        'trade': trade
+    }
+    return render(request, 'log/details.html', data)
