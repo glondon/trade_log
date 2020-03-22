7c7e0bba858e8d278abb60b7c783a6dfc2e955a4
glondon <gregdlondon@gmail.com>
2020-03-21 20:47:59 -0500

    #294 added target and bail area to query

--- invest.py
+++ invest.py
@@ -16,7 +16,8 @@ class Invest:
             utils.exit_app()
 
     def get_investments(self):
-        q = "SELECT id, symbol, entry_price, shares, dividend FROM " + self.invest_tbl + " WHERE status = 'open'"
+        q = "SELECT id, symbol, entry_price, shares, dividend, target, bail_area"
+        q += " FROM " + self.invest_tbl + " WHERE status = 'open'"
         r = self.db.run_query(q)
         if r == False:
             return False
@@ -30,6 +31,8 @@ class Invest:
             items['entry_price'] = row[2]
             items['shares'] = row[3]
             items['div'] = row[4]
+            items['target'] = row[5]
+            items['bail_area'] = row[6]
             items['adjs'] = []
             a = self.get_inv_adjusts(row[0])
             if a != False:
@@ -64,7 +67,7 @@ class Invest:
             shares = 0
             amount = 0
             shares += p['shares']
-            amount = p['entry_price'] * p['shares']
+            amount += p['entry_price'] * p['shares']
 
             for a in p['adjs']:
                 shares += a['shares']
@@ -77,5 +80,5 @@ class Invest:
             items['avg_price'] = round(amount / shares, 2)
 
             positions.append(items)
-            
+
         return positions
